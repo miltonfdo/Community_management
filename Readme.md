e1595dc1a637211e9c430afd68689ecbfc5ebfa6
miltonfdo <milton.fernando@live.com>
2022-06-16 10:54:04 -0500

    commiting all changes

--- Readme.md
+++ Readme.md
@@ -1,2 +1,18 @@
-# Community_management
- 
+Setting up the project:
+This project is created on Python 3.10.2
+
+Step 1 : Setup python virtual environment
+  Command syntax -> python -m venv <name_of_virtualenv>
+  Command -> python -m venv pyEnv
+
+Step 2: Activate the Virtual environment
+ Command -> .\pyEnv\Scripts\activate 
+
+
+Step 3: Install are dependent python packages
+ Command -> pip install -r requirements.txt
+
+
+Note:
+ If any new package is being added newly in the project update the requitemnt.txt file, this can automatically be done by running below command
+  -> pip freeze > requirements.txt
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import csv
+from xmlrpc.client import Boolean
 import pandas as pd
 from tabulate import tabulate
 
@@ -19,9 +20,7 @@ def getCommunityDetails():
         print(f'Processed {line_count} lines.')
 
         pd.options.display.max_rows = 9999
-
         df = pd.read_csv('Blocks.csv')
-        #print(df) 
         print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
 
 
@@ -35,32 +34,84 @@ def getCommunityDetails():
                 BHK=1
                 maxBHK=0
                 TotalAppartment=0
-                TotalOccupied=0 
+                TotalOccupied=0
                 TotalUnoccupied=0
-                
-                
+
+
                 Blocks = int(input("Total number of Blocks in community:"))
-                fieldnames = ['Blocks','BHK','TotalAppartments','TotalOccupied','TotalUnoccupied']
+                fieldnames = ['Block','BHK','TotalAppartments','TotalOccupied','TotalUnoccupied']
                 Blocks_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
                 Blocks_writer.writeheader()
 
                 for block in range(Blocks):
-                   
+
                     print("what is the maximum BHK in Block ",str(block+1))
                     maxBHK=int(input ("Example- 3:  "))
                     for bhk in range(maxBHK):
-                      
+
                         print("Total Number of ",str(bhk+1),"BHK in Block ",str(block+1)," :")
                         TotalAppartment=int(input ("Example - 2:  "))
 
                         print("Total Number of ",str(bhk+1),"BHK Occupied in Block ",str(block+1),":")
                         TotalOccupied=int(input (" :"))
-                        
+
                         TotalUnoccupied=TotalAppartment-TotalOccupied
 
-                        Blocks_writer.writerow({'Blocks':block+1,'BHK':bhk+1,'TotalAppartments':TotalAppartment,'TotalOccupied':TotalOccupied,'TotalUnoccupied':TotalUnoccupied})
+                        Blocks_writer.writerow({'Block':block+1,'BHK':bhk+1,'TotalAppartments':TotalAppartment,'TotalOccupied':TotalOccupied,'TotalUnoccupied':TotalUnoccupied})
+
+def CreateflatOccupancy():
+    Blocks=0
+    FlatNumber=0
+    BHK=0 
+    maxBHK=0
+    totalAppartments=0
+    IsOccupied=False
+    Occupants=''
+
+    with open('Appartments.csv', mode='w') as csv_file:
+        fieldnames = ['Block','FlatNumber','BHK','IsOccupied','Occupants']
+        Blocks_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        Blocks_writer.writeheader()
+
+        Blocks = int(input("Total number of Blocks in community:"))
+    
+        for block in range(Blocks):
+            print("Enter the maximum BHK in Block",str(block+1))
+            maxBHK=int(input(">"))
+            for bhk in range(maxBHK):
+                print("Total Number of ",str(bhk+1),"BHK in Block ",str(block+1)," :")
+                totalAppartments=int(input(">"))
+                for apt in range(totalAppartments):
+                    FlatNumber=str(input("Enter appartment number "))
+                    IsOccupied=Boolean(input("Is appartment Occupied? input ->Yes or NO :"))
+                    Occupants=str(input("Enter Occupants Name:"))
+    
+                    Blocks_writer.writerow({'Block':block+1,'FlatNumber':FlatNumber,'BHK':bhk+1,'IsOccupied':IsOccupied,'Occupants':Occupants})
+    
+                    
+def getFlatOccupancy():
+     df = pd.read_csv('Appartments.csv')
+     print(tabulate(df, headers = 'keys', tablefmt = 'psql',showindex=False))
+     dfOccupancy=df.groupby(['Block','IsOccupied'], as_index=False)["FlatNumber"].count()
+     dfOccupancy.rename(columns={'FlatNumber': 'TotalCount'}, inplace=True)
+
+     print(tabulate(dfOccupancy, headers = 'keys', tablefmt = 'psql',showindex=False))
+
+
+
+    
+    
 
 
 
 
-getCommunityDetails()
+
+
+def main():
+    #getCommunityDetails()
+    #CreateflatOccupancy()
+    getFlatOccupancy()
+
+
+if __name__ == "__main__":
+    main()
